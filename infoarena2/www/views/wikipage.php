21ef005b37367985eecb108d9f58ac672d1ceb3c
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-07-27 18:48:25 +0000

    Cleaned up wiki page viewer
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@62 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/views/wikipage.php
+++ infoarena2/www/views/wikipage.php
@@ -6,13 +6,18 @@
 
 include('header.php');
 
-if (!isset($view['wikipage'])) {
-    echo '<div class="error">Error: no page name</div>';
-}
-else {
-    echo wiki_process_page($view['wikipage'], $view);
-}
+?>
+    <ul id="wikiops">
+        <li><a href="<?= url('edit/' . $view['wikipage']) ?>">Editeaza</a></li>
+        <li><a href="<?= url('history/' . $view['wikipage']) ?>">Vezi istoria</a></li>
+        <li><a href="<?= url('attachment/create' . $view['wikipage']) ?>">Ataseaza</a></li>
+        <li><a href="<?= url('delete/' . $view['wikipage']) ?>">Sterge</a></li>
+    </ul>
+<?php
 
+echo '<h3 id="wikititle">View Generic page <i>'.$view[wikipage]."</i></h1>";
+echo wiki_process_text($view['wikitext'], null);
+echo 'Last modification by ' . $view['last-editor'];
 
 include('footer.php');
 
--- infoarena2/www/wiki/wiki.php
+++ infoarena2/www/wiki/wiki.php
@@ -2,15 +2,6 @@
 
 require_once("Textile.php");
 
-// Process a given page, by name. Return html block.
-function wiki_process_page($wiki_page, $parameters) {
-    // FIXME: write me
-    // FIXME: mysql here.
-    // get the latest version of the page.
-    // wiki_process_text the latest page.text
-    return wiki_process_text("_page_ *{$wiki_page}*", $parameters);
-}
-
 // This processes a big chunk of wiki-formatted text and returns html.
 // The paramaters is an array of usefull information. macros can use them.
 function wiki_process_text($wiki_text, $parameters) {
