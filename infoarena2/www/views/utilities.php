9308dcef89ad7979889a8a67e1349d1679aad40a
strat.cristian@gmail.com <strat.cristian@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-07-28 14:16:53 +0000

    Added ferr_span helper
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@86 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/views/utilities.php
+++ infoarena2/www/views/utilities.php
@@ -5,6 +5,19 @@ function fval($paramName) {
     return htmlentities(getattr($view['form_values'], $paramName));
 }
 
+function ferr_span($paramName) {
+    global $error;
+    if (isset($error[$paramName])) {
+        echo '<span class="fieldError">' . htmlentities($error[$paramName])
+             . '</span>';
+
+        return htmlentities($error[$paramName]);
+    }
+    else {
+        return null;
+    }
+}
+
 function ferr($paramName) {
     global $view;
     return htmlentities(getattr($view['form_errors'], $paramName));
