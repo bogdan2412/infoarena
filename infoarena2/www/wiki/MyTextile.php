2385d392ec1c7db077707057b5b3a86d14c99bb1
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-07-28 18:07:15 +0000

    Separated MyTextile class
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@101 ddd5704e-cc05-0410-ac10-d31acb56fd8e

---
+++ infoarena2/www/wiki/MyTextile.php
@@ -0,0 +1,46 @@
+<?php
+
+@require_once("Textile.php");
+class MyTextile extends Textile {
+    var $page_name;
+
+
+    function MyTextile($page_name, $options = array()) {
+        $this->page_name = $page_name;
+        Textile::Textile($options);
+    }
+
+    function format_link($args) {
+        if (strlen($args['url']) > 1 && $args['url'][0] == '/') {
+            $args['url'] = url(substr($args['url'], 1));
+        } else {
+            $args['clsty'] .= "(wiki_link_external)";
+        }
+        $res = parent::format_link($args);
+
+        return $res;
+    }
+
+    function format_image($args) {
+        $srcpath = $args['src'];
+        if (strlen($srcpath) > 1 && $srcpath[0] == '?') {
+            $file_name = substr($srcpath, 1);
+            $args['src'] = url($page_name,
+                    array('action' => 'download', 'file' => $file_name)); 
+        } else if (strlen($srcpath) > 1 && $srcpath[0] == '/') {
+            $parts = explode('?', substr($srcpath, 1));
+            if (count($parts) == 2) {
+                $other_page_name = $parts[0];
+                $file_name = $parts[1];
+                $args['src'] = url($other_page_name,
+                        array('action' => 'download', 'file' => $file_name)); 
+            }
+        }
+        //echo "<pre>insrc $srcpath outsrc $args[src]</pre>";
+        $res = parent::format_image($args);
+
+        return $res;
+    }
+}
+
+?>
--- infoarena2/www/wiki/wiki.php
+++ infoarena2/www/wiki/wiki.php
@@ -1,48 +1,7 @@
 <?php
 
-error_reporting(0);
-require_once("Textile.php");
-
-class MyTextile extends Textile {
-    var $page_name;
-
-    function MyTextile($page_name, $options = array()) {
-        $this->page_name = $page_name;
-        Textile::Textile($options);
-    }
-
-    function format_link($args) {
-        if (strlen($args['url']) > 1 && $args['url'][0] == '/') {
-            $args['url'] = url(substr($args['url'], 1));
-        } else {
-            $args['clsty'] .= "(wiki_link_external)";
-        }
-        $res = parent::format_link($args);
-
-        return $res;
-    }
-
-    function format_image($args) {
-        $srcpath = $args['src'];
-        if (strlen($srcpath) > 1 && $srcpath[0] == '?') {
-            $file_name = substr($srcpath, 1);
-            $args['src'] = url($page_name,
-                    array('action' => 'download', 'file' => $file_name)); 
-        } else if (strlen($srcpath) > 1 && $srcpath[0] == '/') {
-            $parts = explode('?', substr($srcpath, 1));
-            if (count($parts) == 2) {
-                $other_page_name = $parts[0];
-                $file_name = $parts[1];
-                $args['src'] = url($other_page_name,
-                        array('action' => 'download', 'file' => $file_name)); 
-            }
-        }
-        //echo "<pre>insrc $srcpath outsrc $args[src]</pre>";
-        $res = parent::format_image($args);
-
-        return $res;
-    }
-}
+@require_once("Textile.php");
+@require_once("MyTextile.php");
 
 // This processes a big chunk of wiki-formatted text and returns html.
 // The paramaters is an array of usefull information. macros can use them.
@@ -52,5 +11,6 @@ function wiki_process_text($wiki_text, $parameters) {
     $weaver = new MyTextile(getattr($parameters, 'page_name'));
     return $weaver->process($wiki_text);
 }
+
 error_reporting(1 << 16 - 1);
 ?>
