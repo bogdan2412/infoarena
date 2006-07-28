ff32091974ddfa45c0c95f6a296f65784cf91a04
bogdanpasoi@yahoo.com <bogdanpasoi@yahoo.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-07-28 10:18:34 +0000

    :D
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@79 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/views/listattach.php
+++ infoarena2/www/views/listattach.php
@@ -4,8 +4,7 @@
     <?php foreach ($view['attach_list'] as $v) { ?>
     <li>
         <a href="<?= url($view['page_name'],array ('action' => 'download', 'file'=> $v['name'])) ?>">
-        <?= $v['name'] ?></a>
-        <span class="details"><?= ", atasat de ".$v['username']." la ".$v['timestamp'].", " ?></span>
+        <?= $v['name'] ?></a><span class="details"><?= ", atasat de ".$v['username']." la ".$v['timestamp'].", " ?></span>
         <a href="<?= url($view['page_name'],array ('action' => 'delattach', 'file'=> $v['name'])) ?>">
         Sterge </a>
     </li>
