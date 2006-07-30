2ff150411be46712a20e7b0ef9596a37cae1bc6c
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-07-30 23:46:36 +0000

    Refactored recursive wiki code
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@168 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/wiki/wiki.php
+++ infoarena2/www/wiki/wiki.php
@@ -24,4 +24,46 @@ function wiki_process_text($content, $context) {
     error_reporting(65535);
 }
 
+// This is just like wiki_process_text, but it's meant for recursive calling.
+// You should use this from macros that include other text blocks.
+//
+// This returns a html block. That html block can be an error div.
+function wiki_process_text_recursive($content, $context) {
+    // This uses some black static magic.
+    // include_count is the number of recursions in this function.
+    // When include_count reaches the maximum level then we set
+    // $include_stop to true and quickly kill the whole stack.
+    // At the end of the stack rewind we return an error message
+    // and set $include_stop
+    static $include_count = 0;
+    static $include_stop = false;
+    ++$include_count;
+
+    if ($include_count > IA_MAX_RECURSIVE_INCLUDES) {
+        // Start unwinding.
+        $include_stop = true;
+        --$include_count;
+        //echo "hit maximum recursion $include_count <br />";
+        return;
+    }
+    //echo "going in level $include_count $args[page]<br />";
+
+    //echo "calling wiki <br />";
+    $res = wiki_process_text($content, $context);
+    //echo "done calling wiki <br />";
+
+    --$include_count;
+    // Unwind
+    if ($include_stop) {
+        if ($include_count == 0) {
+            // Stop unwinding. This is the first include.
+            $include_stop = false;
+            return make_error_div("Prea multe include-uri recursive");
+        } else {
+            return;
+        }
+    }
+    return $res;
+}
+
 ?>
