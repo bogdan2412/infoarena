272d801eff2f44031c8563bc6a106427cea346e8
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-08-01 01:06:45 +0000

    Cleaned up the way jrun is called
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@210 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/eval/ClassicGrader.php
+++ infoarena2/eval/ClassicGrader.php
@@ -7,21 +7,35 @@ class ClassicGrader {
 
     public $time_limit;
 
-    public $has_ok_files;
+    public $memory_limit;
+
+    public $uses_ok_files;
 
     public $unique_output;
 
     public $evaluator;
 
     function __construct($id, $parameters) {
-        assert(is_array($parameters));
-
         $this->task_id = $id;
         $this->evaluator = (string)$parameters['evaluator'];
         $this->test_count = (int)$parameters['tests'];
         $this->time_limit = (int)$parameters['timelimit'];
+        $this->memory_limit = (int)$parameters['memlimit'];
         $this->unique_output = (bool)$parameters['unique_output'];
         $this->has_ok_files = (bool)$parameters['okfiles'];
+
+        if ($this->unique_output == true && $this->has_ok_files == false) {
+            tprint("Task has unique output but no ok files");
+        }
+
+        if ($this->unique_output == true && $this->evaluator != "") {
+            tprint("Task has both unique output and evaluator");
+        }
+    }
+
+    // FIXME: move validation here.
+    static function validate($parameters)
+    {
     }
 
     function handle_test($testno)
@@ -70,10 +84,12 @@ class ClassicGrader {
             $result->message = "Eroare de compilare";
             $result->log = "Eroare de compilare:\n" . $compiler_messages;
         } else {
-            $result->log = "Compilare:\n" . $compiler_messages;
+            $result->log = "Compilare:\n" . $compiler_messages . "\n";
         }
 
-        for ($testno = 1; $testno < $this->test_count; ++$testno) {
+        for ($testno = 1; $testno <= $this->test_count; ++$testno) {
+            $result->log .= "\nRulez testul $testno: ";
+
             if (!chdir(IA_EVAL_DIR)) {
                 tprint("Can't chdir to eval dir.");
                 return JobResult::SystemError();
@@ -93,20 +109,60 @@ class ClassicGrader {
             }
 
             if (!copy(IA_EVAL_TEMP_DIR . 'user', IA_EVAL_JAIL_DIR . 'user')) {
-                tprint("Failed copying test $testno");
+                tprint("Failed copying user program");
                 return JobResult::SystemError();
             }
-
             @system("chmod a+x user", $res);
             if ($res) {
                 tprint("Failed to chmod a+x user program");
                 return JobResult::SystemError();
             }
-            
-            $time = $this->time_limit * 1000;
-            $memory = 640000;
-            $jrun_msg = jail_run('user', $time, $memory);
+         
+            // Run user program.
+            $jrunres = jail_run('user', $this->time_limit, $this->memory_limit);
+            tprint("JRUN: ".$jrunres['result'].": ".$jrunres['message']);
+            if ($jrunres['result'] == 'ERROR') {
+                return JobResult::SystemError();
+            } else if ($jrunres['result'] == 'FAIL') {
+                $result->log .= "eroare: ".$jrunres['message'].": 0 puncte";
+                continue;
+            } else {
+                $result->log .= "ok: timp ".$jrunres['time']."ms ".
+                        $jrunres['memory']."kb: ";
+            }
+
+            // Copy ok file, if used.
+            if ($this->has_ok_files) {
+                if (!copy(IA_GRADER_DIR . $this->task_id . '/test' . $testno . '.ok',
+                            IA_EVAL_JAIL_DIR . $this->task_id . '.ok')) {
+                    tprint("Failed copying test $testno of file");
+                    return JobResult::SystemError();
+                }
+            }
+
+            if ($this->has_unique_output) {
+                tprint("Nu stiu ce sa fac cu output unic");
+                return JobResult::SystemError();
+            } else {
+                // Custom grader.
+                if (!copy(IA_EVAL_TEMP_DIR . 'eval', IA_EVAL_JAIL_DIR . 'eval')) {
+                    tprint("Failed copying custom grader");
+                    return JobResult::SystemError();
+                }
+                @system("chmod a+x eval", $res);
+                if ($res) {
+                    tprint("Failed to chmod a+x custom grader");
+                    return JobResult::SystemError();
+                }
+
+                // FIXME: Run grader here.
+                $score = 100 / $this->test_count;
+                $result->score += $score;
+                $result->log .= "bonus de la fluffy: $score puncte";
+            }
         }
+
+        $result->log .= "\n\nPunctaj total: {$result->score}\n";
 
         return $result;
     }
--- infoarena2/eval/utilities.php
+++ infoarena2/eval/utilities.php
@@ -63,8 +63,60 @@ function compile_file($file_name, &$compiler_message)
     return true;
 }
 
-function jail_run($program, &$time_limit, &$memory_limit)
+// Parses jrun output.
+// Returns an array with result, time, memory and message.
+//
+// Result is 'OK', 'FAIL' or 'ERROR'
+// If result is ERROR time and memory are not available
+// Returns false on error.
+function jrun_parse_message($message)
 {
+    if (!preg_match("/^(ERROR|FAIL|OK):\ (.*)$/", $message, $matches)) {
+        return false;
+    }
+
+    $res = array();
+    $res['result'] = $matches[1];
+    $res['message'] = $matches[2];
+    if ($matches[1] == 'OK' || $matches[1] == 'FAIL') {
+        if (!preg_match("/^time\ ([0-9]+)ms\ memory\ ([0-9]+)kb:\ (.*)$/",
+                    $res['message'], $matches)) {
+            return false;
+        } else {
+            $res['time'] = (int)$matches[1];
+            $res['memory'] = (int)$matches[2];
+            $res['message'] = $matches[3];
+        }
+    }
+
+    // Trim . .\n and other stupid shit like that.
+    $res['message'] = preg_replace("/\s*\.?\n?^/i", "", $res['message']);
+    return $res;
+}
+
+// Run a program in a special jail environment.
+// It calls an external jailer and parses output.
+//
+// $time and $memory contain the time and memory limits (or false).
+// if $capture_std is true the it will ask jrun to capture user program
+// stdin/stdout.
+//
+// The return value is an array:
+//      result: OK:    program ran perfectly
+//              FAIL:  program failed for various reasons.
+//              ERROR: internal error (user program not to blame).
+//      message: an explanatory string.
+//      time, memory: Amount of time and memory the program used.
+//      stdin, stderr: Contents of user program standard i/o.
+//               Only if $capture_std is true.
+//
+// If result is ERROR time, memory, stdin and stdout are never set.
+function jail_run($program, $time, $memory, $capture_std = false)
+{
+    if ($capture_std) {
+        tprint("I don't know how to capture stdio/stdout");
+    }
+
     $cmdline = IA_JRUN_PATH;
     $cmdline .= " --prog=./" . $program;
     $cmdline .= " --dir=" . IA_EVAL_JAIL_DIR;
@@ -74,22 +126,25 @@ function jail_run($program, &$time_limit, &$memory_limit)
     if (defined(IA_EVAL_JAIL_GID)) {
         $cmdline .= " --gid=" . IA_EVAL_JAIL_GID;
     }
-    if (isset($time_limit)) {
-        $cmdline .= " --time-limit=" . $time_limit;
+    if (isset($time)) {
+        $cmdline .= " --time-limit=" . $time;
     }
-    if (isset($memory_limit)) {
-        $cmdline .= " --memory-limit=" . $memory_limit;
+    if (isset($memory)) {
+        $cmdline .= " --memory-limit=" . $memory;
     }
 
     ob_start();
-    @system($cmdline);
+    @system($cmdline, $res);
     $message = ob_get_contents();
     ob_end_clean();
 
-    tprint("Executed $cmdline");
-    tprint("Got back $message");
+    if ($res) {
+        return array('result' => 'ERROR', 'message' => 'Failed executing jail');
+    }
 
-    return $message;
+    $result = jrun_parse_message($message);
+
+    return $result;
 }
 
 ?>
