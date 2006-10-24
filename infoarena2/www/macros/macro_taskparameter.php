1efdf62d789b40f0130d30e41864bb710d863b0b
strat.cristian@gmail.com <strat.cristian@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-10-24 16:50:52 +0000

    Dummy commit so I can rename file macro_taskparameter.php
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@260 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/macros/macro_taskparameter.php
+++ infoarena2/www/macros/macro_taskparameter.php
@@ -1,33 +1,63 @@
 <?php
 
-function macro_taskparameter($args)
-{
-    $context = $args['context'];
-    if (!isset($context['task'])) {
-        return make_error_div("This macro only works in task pages.");
-    }
-    $params = $context['task_parameters'];
+// Displays a task field, be it a hard-coded field such as task author or a grader parameter such as `timelimit`.
+// NOTE: The macro employs a simple caching mechanism (via static variables, cache expires at the end of the request)
+//       to avoid multiple database queries.
+//
+// Arguments:
+//      task_id (required)            Task identifier (without task/ prefix)
+//      param (required)              Parameter name. See the source code for possible values.
+//      default_value (optional)      Display this when no such parameter is found
+//
+// Examples:
+//      TaskParam(task_id="adunare" param="author")
+//      TaskParam(task_id="adunare" param="timelimit")
+function macro_taskparam($args) {
+    static $last_task_id = null;
+    static $task;
+    static $params;
 
-    if (!isset($args['param'])) {
-        return make_error_div("This macro needs a 'param' argument.");
-    }
-    $param = $args['param'];
+    $task_id = getattr($args, 'task_id');
+    $param = getattr($args, 'param');
 
-    if ($param == 'author') {
-        return $context['task']['author'];
+    // validate arguments
+    if ($task_id) {
+        return make_error_div("Expecting parameter `task_id`");
     }
-    if ($param == 'source') {
-        return $context['task']['source'];
+    if ($param) {
+        return make_error_div("Expecting parameter `param`");
     }
-    if ($param == 'type') {
-        return $context['task']['type'];
+
+    // fetch task & parameters
+    if ($last_task_id != $task_id) {
+        $task = task_get($task_id);
+        $params = task_get_parameters($task_id);
+        $last_task_id = $task_id;
     }
-    if ($param == 'id') {
-        return $context['task']['id'];
+
+    // validate task id
+    if (!$task) {
+        return make_error_div("Invalid task identifier");
     }
-    if (!isset($params[$param])) {
-        return make_error_div("Task doesn't have parameter '$param'");
+
+    // serve desired value
+    switch ($param) {
+        case 'author':
+            return $task['author'];
+
+        case 'source':
+            return $task['source'];
+
+        case 'type':
+            return $task['type'];
+
+        case 'id':
+            return $task['id'];
+
+        default:
+            if (!isset($params[$param])) {
+                return make_error_div("Task doesn't have parameter '$param'");
+            }
     }
-    return $params[$param];
 }
 ?>
