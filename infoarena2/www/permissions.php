cd772492f9eb8aa31f98449a2ac8c07613db58a6
strat.cristian@gmail.com <strat.cristian@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-11-01 13:07:55 +0000

    Dummy commit
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@264 ddd5704e-cc05-0410-ac10-d31acb56fd8e

---
+++ infoarena2/www/permissions.php
@@ -0,0 +1,212 @@
+<?php
+// This module implements permission policy (regulates who can do what on the website).
+//
+// You tipically don't interact with the functions below but use identity_require() and identity_can()
+// from `identity.php` to enforce proper permissions.
+
+// We distinguish between 4 types of users:
+//  - anonymous     non-authenticated visitors
+//  - normal        registered & authenticated users
+//  - reviewer      users who are allowed to contribute with content & new (hidden) tasks but are not
+//                  trusted enough to access administrative panels.
+//  - admin         can do anything, including starting/stopping contests and viewing any hidden task/contest
+
+
+
+
+
+// Returns boolean whether $user can perform $action $ontoObject.
+//
+//      $user           is instance of user model as returned by user_get*()
+//      $action         convention. see source code for valid actions
+//      $ontoObject     hacky, magically un-typed, usually a model instance as returned by db.php functions.
+//                      However, some actions may expect it to be null or any arbitrary type.
+function permission_query($user, $action, $ontoObject) {
+    // split actions (wiki-view) into an action group (wiki) and action (view)
+    list($group, $action) = split("-", $action);
+
+    // group dispatcher
+    switch ($group) {
+        case 'user':
+            return permission_user($user, $action, $ontoObject);
+
+        case 'wiki':
+        case 'news':
+            return permission_wiki($user, $action, $ontoObject);
+
+        case 'round':
+            return permission_round($user, $action, $ontoObject);
+
+        case 'task':
+            return permission_task($user, $action, $ontoObject);
+
+        case 'attach':
+            return permission_attach($user, $action, $ontoObject);
+
+        case 'macro':
+            return permission_macro($user, $action, $ontoObject);
+
+        case 'specialpage':
+            return permission_specialpage($user, $action, $ontoObject);
+
+        default:
+            log_error('Invalid action group: "' . $group . '"');
+            return false;
+    }
+}
+
+function permission_user($user, $action, $ontoUser) {
+    switch ($action) {
+        case 'login':
+            // only anonymous users can login
+            return is_null($user);
+
+        case 'logout':
+            // only authenticated users can logout
+            return !is_null($user);
+
+        case 'viewprofile':
+            // anyone can view anyone's profile
+            return true;
+
+        case 'editprofile':
+            // anyone can edit their own profile. admins can edit any profile
+            return $user && $ontoUser && ($user['id'] == $ontoUser['id'] || 'admin' == $user['security_level']);
+        
+        default:
+            log_error('Invalid user action: '.$action);
+            return false;
+    }
+}
+
+function permission_wiki($user, $action, $textblock) {
+    switch ($action) {
+        case 'view':
+        case 'history':
+            // anyone can view a wiki page & its history
+            return true;
+
+        case 'edit':
+        case 'create':
+        case 'restore':
+            // admins & reviewers can edit/create/restore wiki pages
+            return ('admin' == $user['security_level'] || 'reviewer' == $user['security_level']);
+
+        default:
+            log_error('Invalid wiki action: '.$action);
+            return false;
+    }
+}
+
+function permission_attach($user, $action, $attach) {
+    switch ($action) {
+        case 'download':
+        case 'list':
+            // anyone can download/list attachments
+            return true;
+
+        case 'create':
+        case 'overwrite':
+            // only reviewers/admins can create/replace attachments
+            return ('admin' == $user['security_level'] || 'reviewer' == $user['security_level']);
+
+        case 'delete':
+            // Only admins can delete attachments for good.
+            // Since deleted attachments are gone for good, we allow this action only to administrators
+            // in order to control/harden an eventual damage caused by a reviewer "gone bad".
+            return $attach && ('admin' == $user['security_level']);
+
+        default:
+            log_error('Invalid attach action: '.$action);
+            return false;
+    }
+}
+
+function permission_task($user, $action, $task) {
+    $level = getattr($user, 'security_level');
+
+    switch ($action) {
+        case 'view':
+        case 'history':
+            // anyone can view public tasks; reviewers can also view their own hidden tasks; admins view everything
+            return $task && (!$task['hidden'] || 'admin'==$level || ($user && 'reviewer'==$level && $task['user_id']==$user['id']));
+
+        case 'submit':
+            // any authenticated user can submit to public tasks; reviewers can also submit to their own hidden tasks; admins submit to everything
+            if (!$user) {
+                // no anonymous, please
+                return false;
+            }
+            return $task && (!$task['hidden'] || 'admin'==$level || ('reviewer'==$level && $task['user_id']==$user['id']));
+
+        case 'edit':
+        case 'restore':
+            // reviewers edit their own tasks; admins edit everything
+            return $user && $task && ('admin'==$level || ('reviewer'==$level && $task['user_id']==$user['id']));
+
+        case 'create':
+            // reviewers & admins can create tasks
+            return $user && ('admin'==$level || 'reviewer'==$level);
+
+        case 'publish':
+            // only admins can publish tasks (un-hide them)
+            return $user && ('admin'==$level);
+
+        default:
+            log_error('Invalid task action: '.$action);
+            return false;
+    }
+}
+
+function permission_round($user, $action, $round) {
+    $level = getattr($user, 'security_level');
+
+    switch ($action) {
+        case 'view':
+        case 'history':
+            // anyone can view a round (contest) page and its history
+            return true;
+
+        case 'submit':
+            // any authenticated user can submit to an active round; reviewers & admins can submit to any round
+            // Please note there's an additional permission query for submitting to a given task.
+            return $user && $round && ($round['active'] || 'admin'==$level || 'reviewer'==$level);
+
+        case 'edit':
+        case 'create':
+        case 'restore':
+            // only administrators edit/create round pages
+            return $user && ('admin'==$level);
+
+        default:
+            log_error('Invalid task action: '.$action);
+            return false;
+    }
+}
+
+function permission_specialpage($user, $action, $textblock) {
+    switch ($action) {
+        case 'index':
+            // only admins & reviewers can view page index
+            // security precaution
+            return $user && ('admin'==$level || 'reviewer'==$level);
+
+        default:
+            log_error('Invalid special page action: '.$action);
+            return false;
+    }
+}
+
+function permission_macro($user, $action, $args) {
+    switch ($action) {
+        case 'debug':
+            // only administrators & reviewers can execute a debugging macro
+            return $user && ('admin'==$level || 'reviewer'==$level);
+        
+        default:
+            log_error('Invalid macro action: '.$action);
+            return false;
+    }
+}
+
+?>
