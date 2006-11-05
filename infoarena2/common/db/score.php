5e9d00b574869c198e81ec33ce6d0a600cc20fda
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-11-05 15:59:35 +0000

    Updated rankings macro and score_get
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@301 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/common/db/score.php
+++ infoarena2/common/db/score.php
@@ -16,27 +16,60 @@ function score_update($rank_id, $user_id, $task_id, $round_id, $value)
     return db_query($query);
 }
 
-// Get scores.
-// $user_id, $task_id, $round_id can be null.
-function score_get($rank_id, $user_id, $task_id, $round_id, $start, $count, $groupby = "user_id")
+function db_escape_array($array)
 {
+    $ret = '';
+    log_print($array);
+    foreach ($array as $element) {
+        if ($ret) {
+            $ret .= ", ";
+        }
+        $ret .= "'" . db_escape($element) . "'";
+    }
+    return $ret;
+}
+
+// Get scores.
+// $user, $task, $round can be null.
+function score_get($rank_id, $user, $task, $round, $start, $count, $groupby = "user_id")
+{
+    // Build WHERE.
     $where = array();
-    if ($user_id != null) {
-        $where[] = sprintf("`user_id` == '%s'", $user_id);
+/*    if ($user != null) {
+        if (is_array($user) && count($user) > 0) {
+            $where[] = "(`user_id` IN (" . db_escape_array($user) . "))";
+        } else if (is_string($user)) {
+            $where[] = sprintf("(`user_id` == '%s')", $user);
+        }
+    }*/
+    if ($task != null) {
+        if (is_array($task) && count($task) > 0) {
+            $where[] = "(`task_id` IN (" . db_escape_array($task) . "))";
+        } else if (is_string($task)) {
+            $where[] = sprintf("(`task_id` = '%s')", $task);
+        }
     }
-    if ($task_id != null) {
-        $where[] = sprintf("`task_id` == '%s'", $task_id);
+    if ($round != null) {
+        if (is_array($round) && count($round) > 0) {
+            log_print(count($round));
+            $where[] = "(`round_id` IN (" . db_escape_array($round) . "))";
+        } else if (is_string($round)) {
+            $where[] = sprintf("(`round_id` = '%s')", $round);
+        }
     }
-    if ($round_id != null) {
-        $where[] = sprintf("`round_id` == '%s'", $round_id);
-    }
+
     log_assert($rank_id !== null);
-    $where[] = sprintf("`rank_id` == '%s'", $rank_id);
+    $where[] = sprintf("ia_score.`id` = '%s'", db_escape($rank_id));
     $query = sprintf("
-            SELECT `id` as `rank_id`, `user_id`, `task_id`, `round_id`, SUM(`round_id`)
-            FROM ia_score
-            WHERE %s GROUP BY %s LIMIT %s, %s",
+            SELECT ia_score.`id` as `rank_id`, `user_id`, `task_id`, `round_id`, SUM(`score`) as score, 
+                ia_user.username as user_name, ia_user.full_name as user_full
+            FROM ia_score 
+                LEFT JOIN ia_user ON ia_user.id = ia_score.user_id
+            WHERE %s GROUP BY %s 
+            ORDER BY `score` DESC LIMIT %s, %s",
             join($where, " AND "), $groupby, $start, $count);
+    log_print($query);
+    return db_fetch_all($query);
 }
 
 ?>
--- infoarena2/www/macros/macro_rankings.php
+++ infoarena2/www/macros/macro_rankings.php
@@ -14,51 +14,22 @@ require_once("format_table.php");
 //      Rankings(rounds="preONI2007/1/a | preONI2007/2/a" count="10")
 function macro_rankings($args) {
     // How many rows to display at a time.
-    $display_rows = getattr($args, 'count', "0");
+    $display_rows = getattr($args, 'count', IA_DEFAULT_ROWS_PER_PAGE);
     if (!preg_match('/^[0-9]{1,4}$/', $display_rows)) {
         return macro_error("Invalid count parameter.");
     }
 
-    // Make a list of round ids
-    $roundStr = getattr($args, 'rounds', '');
-    $rounds = preg_split('/\s*\|\s*/', $roundStr);
-
-    // construct query
-    //  - first, we need a WHERE condition for round_id.
-    //    goal:  '<round-id-1>', '<round-id-2>' ... '<round-id-n>'
-    $whereRound = '';
-    foreach ($rounds as $round_id) {
-        if ($whereRound) {
-            $whereRound .= ', ';
-        }
-        $whereRound .= "'" . db_escape($round_id) . "'";
-    }
-
-    //  - SQL frame
-    $query = "
-        SELECT
-            user_id, ia_user.username AS `username`,
-            ia_user.full_name AS full_name, SUM(`score`) AS totalScore
-        FROM ia_score
-        LEFT JOIN ia_user ON ia_user.id = ia_score.user_id
-        WHERE round_id IN (%s)
-        GROUP BY user_id
-        ORDER BY totalScore DESC
-    ";
-    if ($display_rows != 0) {
-        $query .= " LIMIT 0, $display_rows";
-    }
-    $query = sprintf($query, $whereRound);
-
     // query database
-    $rankings = db_fetch_all($query);
+    $roundstr = getattr($args, 'rounds', '');
+    if ($roundstr == '') {
+        return macro_error("Parameters 'rounds' is required.");
+    }
+    $rounds = preg_split('/\s*\|\s*/', $roundstr);
+    $rankings = score_get("score", null, null, $rounds, 0, $display_rows);
 
-    log_backtrace();
     $column_infos = array(
-            array('title' => 'ID', 'key' => 'user_id'),
-            array('title' => 'User', 'key' => 'username'),
-            array('title' => 'Nume', 'key' => 'full_name', 'rowform' => '_format_full_name'),
-            array('title' => 'Scor', 'key' => 'totalScore'),
+            array('title' => 'Nume', 'key' => 'user_full', 'rowform' => '_format_full_name'),
+            array('title' => 'Scor', 'key' => 'score'),
     );
     $options = array(
             'display_rows' => $display_rows,
@@ -69,7 +40,7 @@ function macro_rankings($args) {
 
 // Function for printing user link.
 function _format_full_name($row) {
-    return "<a href='".url('user/' . $row['username'])."'>{$row['full_name']}</a></td>";
+    return "<a href='".url('user/' . $row['user_name'])."'>{$row['user_full']}</a></td>";
 }
 
 ?>
