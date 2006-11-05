52a0a6cc699419e0b239748efc51b20655e85b07
svalentin <svalentin@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-11-05 17:29:16 +0000

    minor bug-fix in case you didn't specify a topicID
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@309 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/smf/ia_recentposts.php
+++ infoarena2/smf/ia_recentposts.php
@@ -1,6 +1,7 @@
 <?php 
     require("./SSI.php");
 
+    if (!isset($_GET['topicID'])) return ;
     ssi_recentPostsFromTopic($_GET['topicID'], 
                              isset($_GET['num']) ? $_GET['num'] : 8);
  ?>
