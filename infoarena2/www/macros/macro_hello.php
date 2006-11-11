0427cac9190660726179d2db837083945542cea2
strat.cristian@gmail.com <strat.cristian@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-11-11 17:02:28 +0000

    Fixed macro hello
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@337 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/macros/macro_hello.php
+++ infoarena2/www/macros/macro_hello.php
@@ -3,10 +3,9 @@
 // This is called from textile like
 // ==Hello(key1 = value1 key2 = value2)==
 // There is an extra global argument called page_name.
-function macro_hello($args)
-{
+function macro_hello($args) {
     $target = getattr($args, 'target', 'anonim');
-    return '<b>Hello '.$target.' from '.$args['context']['page_name'].'</b>';
+    return '<strong>Hello, '.$target.'</strong>';
 }
 
 ?>
