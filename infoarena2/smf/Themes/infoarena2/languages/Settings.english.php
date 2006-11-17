139647d9e8d42b79e04a4cb7c375ecac3fc8fd1a
strat.cristian@gmail.com <strat.cristian@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-11-17 16:51:23 +0000

    smf in SMF!!!
    tabula rasa
    
    
    
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@381 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/smf/Themes/infoarena2/languages/Settings.english.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Version: 1.1 RC2; Settings
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.jpg';
-$txt['theme_description'] = 'blackTed Skin Refreshed designed by <a href="http://www.pixelslot.com/">PixelSlot.</a> (c) 2006 PixelSlot';
-
-?>
