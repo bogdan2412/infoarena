1e5aedee0ed3c5ce4adf162aab13ec9f068b369a
iahack <iahack@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-12-04 05:02:20 +0000

    Creative Commons license
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@501 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/views/sitewide.php
+++ infoarena2/www/views/sitewide.php
@@ -55,13 +55,33 @@ function ia_template_footer() {
 ?>
 <div id="footer">
     <ul class="clear">
-        <li><a href="<?= htmlentities(url('', array(), true)) ?>">Prima pagina</a></li>
+        <li>&copy;&nbsp;2006&nbsp;-&nbsp;<a href="<?= htmlentities(url('Asociatia-infoarena', array(), true)) ?>">Asociatia infoarena</a></li>
+        <li class="separate"><a href="<?= htmlentities(url('', array(), true)) ?>">Prima pagina</a></li>
         <li><a href="<?= htmlentities(url('Despre-infoarena', array(), true)) ?>">Despre infoarena</a></li>
         <li><a href="<?= htmlentities(url('Termeni-si-conditii', array(), true)) ?>">Termeni si conditii</a></li>
         <li><a href="<?= htmlentities(url('Contact', array(), true)) ?>">Contact</a></li>
         <li class="top"><a href="#header">Sari la inceputul paginii &uarr;</a></li>
     </ul>
-    <p>&copy; 2006 - <a href="<?= htmlentities(url('Asociatia-infoarena', array(), true)) ?>">Asociatia infoarena</a></p>
+    <p class="cc">
+    <!--Creative Commons License-->
+    <a class="badge" rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/"><img alt="Creative Commons License" src="http://i.creativecommons.org/l/by-nc/2.5/88x31.png"/></a>
+    Cu exceptia cazurilor in care se specifica altfel, continutul site-ului infoarena<br/>este publicat sub licenta <a rel="license" href="http://creativecommons.org/licenses/by-nc/2.5/">Creative Commons Attribution-NonCommercial 2.5</a>.
+    <!--/Creative Commons License-->
+    <!-- <rdf:RDF xmlns="http://web.resource.org/cc/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#">
+        <Work rdf:about="">
+            <license rdf:resource="http://creativecommons.org/licenses/by-nc/2.5/" />
+        </Work>
+        <License rdf:about="http://creativecommons.org/licenses/by-nc/2.5/">
+            <permits rdf:resource="http://web.resource.org/cc/Reproduction"/>
+            <permits rdf:resource="http://web.resource.org/cc/Distribution"/>
+            <requires rdf:resource="http://web.resource.org/cc/Notice"/>
+            <requires rdf:resource="http://web.resource.org/cc/Attribution"/>
+            <prohibits rdf:resource="http://web.resource.org/cc/CommercialUse"/>
+            <permits rdf:resource="http://web.resource.org/cc/DerivativeWorks"/>
+        </License>
+    </rdf:RDF> -->    
+    
+    </p>
 </div>
 
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
