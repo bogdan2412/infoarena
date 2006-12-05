2d903c8e33fb8caaf9b177a803737b1695b16c98
iahack <iahack@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2006-12-05 01:31:27 +0000

    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@513 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- infoarena2/www/views/job_detail.php
+++ infoarena2/www/views/job_detail.php
@@ -10,7 +10,7 @@ require_once(IA_ROOT . 'www/format/format.php');
 <table class="job">
 <tr>
     <th class="task-id">Problema</th>
-    <td class="task-id"><?= htmlentities($job['task_id']) ?></td>
+    <td class="task-id"><?= format_link("problema/".$job['task_id'], $job['task_id']) ?></td>
     <th class="compiler-id">Compilator</th>
     <td class="compiler-id"><?= htmlentities($job['compiler_id']) ?></td>
 </tr>
