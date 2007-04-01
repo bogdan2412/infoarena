9c62fc492703e76ca2e2d7edbb3b0401cb11ba85
bogdanpasoi@yahoo.com <bogdanpasoi@yahoo.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2007-04-01 13:00:14 +0000

    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@701 ddd5704e-cc05-0410-ac10-d31acb56fd8e

---
+++ www/controllers/reeval.php
@@ -0,0 +1,21 @@
+<?php
+require_once(IA_ROOT_DIR."common/db/job.php");
+require_once("job_filters.php");
+
+function controller_reeval() {
+    identity_require('job-reeval');
+    if (!request_is_post()) {
+        flash_error('Nu se poate reevalua.');
+        redirect(url_monitor());
+    }
+    $filters = job_get_filters();
+    $count = job_get_count($filters);
+    if ($count > IA_REEVAL_MAXJOBS) {
+        flash_error('Prea multe job-uri!');
+        redirect(url_monitor());
+    }
+    job_reeval($filters);
+    flash('Se reevalueaza urmatoarele job-uri... ');
+    redirect(url_monitor($filters));
+}
+?>
