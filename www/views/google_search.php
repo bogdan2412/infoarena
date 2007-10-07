20e7c0433138573d2cc5b32a659fd26b853a433a
bogdanpasoi@yahoo.com <bogdanpasoi@yahoo.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2007-10-07 21:50:22 +0000

    Minor fixes.
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@778 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- www/views/google_search.php
+++ www/views/google_search.php
@@ -4,7 +4,7 @@
   <legend><img src="<?= htmlentities(url_static('images/icons/search.png')) ?>" alt="!" /> Cautare</legend>
     <input type="hidden" name="cx" value="010130381492294265836:kw-fmmvpxco" />
     <input type="hidden" name="cof" value="FORID:9" />
-    <input name="q" type="text" size="40" />
+    <input name="q" type="text" size="22" />
     <input type="submit" name="sa" value="Cauta" class="button important"/>
   </fieldset>
   </form>
