1d8105b188783712ab915e1eb3d013340176d794
cdleonard@gmail.com <cdleonard@gmail.com@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2007-12-16 14:06:33 +0000

    Tested attach-fix-size.php
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@862 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- scripts/attach-fix-size.php
+++ scripts/attach-fix-size.php
@@ -1,32 +1,34 @@
 #! /usr/bin/env php
 <?php
-// This script is used to repair bad filenames in the IA_ROOT_DIR/attach folder.
-// Previously infoarena lowercased the filename for any attachment making "test" and "Test" 
-// having the same file in the attach folder. This script tries to fix this behavior.
 
 require_once(dirname($argv[0]) . "/utilities.php");
 require_once(IA_ROOT_DIR."common/db/attachment.php");
 
-ini_set("memory_limit", "128M");
-
 db_connect();
+$fixed = $errors = 0;
 $query = "SELECT * FROM ia_file;";
 $attachments = db_fetch_all($query);
-$fixed = $errors = 0;
 
-log_print("Exista ".count($attachments)." atasamente...");
+log_print("There are ".count($attachments)." attachments in the database...");
 foreach ($attachments as $attach) {
-    log_print('Verific '.$attach['page'].'\\'.$attach['name']);
+    log_print("Checking {$attach['page']}/{$attach['name']}");
     $name = attachment_get_filepath($attach);
     if (!file_exists($name)) {
-        log_warn('Fisierul '.$name.' nu exista!');
+        log_warn("File $name doens't exist!");
         continue;
     }
     $true_size = filesize($name);
-    if ($attach['size'] != $true_size) {
-        log_print('Repar '.$attach['page'].'\\'.$attach['name']);
-        $query = sprintf("UPDATE ia_file SET size = %s WHERE id = %s", $attach['size'], $attach['id']);
+    $db_size = $attach['size'];
+    if ($db_size != $true_size) {
+        log_print("Fixing {$attach['page']}/{$attach['name']} $db_size != $true_size");
+        $query = sprintf("UPDATE ia_file SET size = %s WHERE id = %s",
+                    $true_size, $attach['id']);
+        db_query($query);
+        log_assert(db_affected_rows() == 1);
         $fixed++;
     }
 }
-log_print("S-au reparat ".$fixed." atasamente!");
+
+log_print("Fixed ".$fixed." attachments!");
+
+?>
