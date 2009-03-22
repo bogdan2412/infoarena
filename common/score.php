c73ea19f7d68f20cb1f5a5e62f30ac7423b28632
bogdan2412 <bogdan2412@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2009-03-22 10:12:00 +0000

    Make eval update score on job's round. It previously updated all the task's parent rounds.
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@1040 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- common/score.php
+++ common/score.php
@@ -4,13 +4,10 @@ require_once(IA_ROOT_DIR . "common/db/score.php");
 require_once(IA_ROOT_DIR . "common/db/round.php");
 require_once(IA_ROOT_DIR . "common/db/task.php");
 
-function score_update_for_job($score, $time, $user_id, $task_id)
+function score_update_for_job($score, $time, $user_id, $task_id, $round_id)
 {
-    $rounds = task_get_parent_rounds($task_id);
-    foreach ($rounds as $round_id) {
-        $round = round_get($round_id);
-        round_event_job_score($round, $score, $time, $user_id, $task_id);
-    }
+    $round = round_get($round_id);
+    round_event_job_score($round, $score, $time, $user_id, $task_id);
 }
 
 function round_event_job_score($round, $score, $time, $user_id, $task_id) {
