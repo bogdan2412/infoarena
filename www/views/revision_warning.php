c8bdea28c18f1e1a7bc46e74f38230d5389ac880
bogdan2412 <bogdan2412@ddd5704e-cc05-0410-ac10-d31acb56fd8e>
2010-01-17 15:48:07 +0000

    When viewing a textblock's revision by it's id, show a different message if it is the last one. Done by Toni
    REVIEW URL: http://reviewboard.infoarena.ro/r/132/
    
    
    
    git-svn-id: https://hackers.infoarena.ro/dav-svn/trunk@1104 ddd5704e-cc05-0410-ac10-d31acb56fd8e

--- www/views/revision_warning.php
+++ www/views/revision_warning.php
@@ -1,26 +1,30 @@
 <div class="warning">
-    Atentie! Aceasta pagina nu este actuala (este varianta de la <?= html_escape($textblock['timestamp']) ?>)</br>
     <?php
+    if ($view['revision'] < $view['revision_count']) {
+        echo "Atenţie! Această pagina nu este actuală (este varianta de la ".html_escape($textblock['timestamp']).")</br>\n";
+    } else {
+        echo "Atenţie! Aceasta este ultima versiune a paginii, scrisă la ".html_escape($textblock['timestamp']).")</br>\n";
+    }
     if ($view['revision'] > 1) {
-        echo format_link(url_textblock_revision($view['page_name'], $view['revision'] - 1), "Revizia anterioara");
+        echo format_link(url_textblock_revision($view['page_name'], $view['revision'] - 1), "Revizia anterioară");
     }
     else {
-        echo "Revizia anterioara";
+        echo "Revizia anterioară";
     }
     ?>
     &nbsp;
     <?php
     if ($view['revision'] < $view['revision_count']) {
-        echo format_link(url_textblock_revision($view['page_name'], $view['revision'] + 1), "Revizia urmatoare");
-    } 
+        echo format_link(url_textblock_revision($view['page_name'], $view['revision'] + 1), "Revizia următoare");
+    }
     else {
-        echo "Revizia urmatoare";
+        echo "Revizia următoare";
     }
     ?>
     &nbsp;
     <?php
     if (identity_can('textblock-delete-revision', $view['textblock'])) {
-        echo format_post_link(url_textblock_delete_revision($view['page_name'], $view['revision']), "Sterge");
+        echo format_post_link(url_textblock_delete_revision($view['page_name'], $view['revision']), "Şterge");
     }
     ?>
 </div>
