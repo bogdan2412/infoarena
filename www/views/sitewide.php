dfec1079dd3a18ead2fb7e6b51a655aaa66e006e
Cătălin Frâncu <cata@francu.com>
2021-01-08 16:16:04 +0200

    [Fixes #23] Ability to bypass Google Analytics tag.

--- www/views/sitewide.php
+++ www/views/sitewide.php
@@ -168,7 +168,7 @@ function ia_template_footer() {
 ?>
 </div>
 
-<?php if (!IA_DEVELOPMENT_MODE) { ?>
+<?php if (!IA_DEVELOPMENT_MODE && GOOGLE_ANALYTICS_TRACKING_ID) { ?>
     <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
     </script>
     <script type="text/javascript">
