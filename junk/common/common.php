0a18c4a49a58bfcb935f05456dc224a536b39771
Cătălin Frâncu <cata@francu.com>
2023-07-01 11:14:30 +0300

    Remove the junk directory.

--- junk/common/common.php
+++
@@ -1,9 +0,0 @@
-// Unroll iterator, stores result in array
-function unroll(Iterator $iterator) {
-    $collect = array();
-    foreach ($iterator as $entry) {
-        $collect[] = $entry;
-    }
-
-    return $collect;
-}
