f3370e56d910c39741cc95fbc5df7e5f1cc11f77
Cătălin Frâncu <cata@francu.com>
2023-07-08 12:53:15 +0300

    Remove old migration scripts.

--- scripts/migrate-interactive-task.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-require_once(dirname($argv[0]) . "/utilities.php");
-db_connect();
-
-$query = "ALTER TABLE ia_task
-          MODIFY type enum('classic','interactive','output-only')";
-
-db_query($query);
