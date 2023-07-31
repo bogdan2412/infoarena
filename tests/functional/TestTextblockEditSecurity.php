b081dc6117270a49d54a48a9ec82a7414bbe9fef
Cătălin Frâncu <cata@francu.com>
2023-07-31 14:39:00 +0300

    [#61] Test changing textblock security.

---
+++ tests/functional/TestTextblockEditSecurity.php
@@ -0,0 +1,46 @@
+<?php
+
+class TestTextblockEditSecurity extends FunctionalTest {
+
+  private array $origPage;
+
+  function run(): void {
+    $this->testHelperCannotEdit();
+    $this->testAdminCanEdit();
+  }
+
+  private function testHelperCannotEdit(): void {
+    $this->login('helper', '1234');
+    $this->visitTextblockEditPage('page-public');
+    $this->assertInputValue('#form_title', 'page-public');
+    $this->assertNoElement('#security_select');
+  }
+
+  private function testAdminCanEdit(): void {
+    $this->login('admin', '1234');
+
+    $this->changePage();
+    $this->verifyPage();
+    $this->restorePage();
+  }
+
+  private function changePage(): void {
+    $this->visitTextblockEditPage('page-public');
+    $this->assertSelectVisibleText('#security_select', 'Public');
+    $this->changeSelect('#security_select', 'Protected');
+    $this->clickButton('Salvează');
+  }
+
+  private function verifyPage(): void {
+    $this->visitTextblockEditPage('page-public');
+    $this->assertSelectVisibleText('#security_select', 'Protected');
+  }
+
+  private function restorePage(): void {
+    sleep(1);
+    $this->visitTextblockEditPage('page-public');
+    $this->changeSelect('#security_select', 'Public');
+    $this->clickButton('Salvează');
+  }
+
+}
