93b1f3fc1cfa0c95d00b0e031ea313429e74ffd5
Cătălin Frâncu <cata@francu.com>
2023-08-07 11:36:16 +0300

    [#61] Test permission to use task in round.

---
+++ tests/functional/TestRoundUseTask.php
@@ -0,0 +1,56 @@
+<?php
+
+class TestRoundUseTask extends FunctionalTest {
+
+  function run(): void {
+    $this->createRound();
+    $this->testInternCannotUsePrivateTask();
+    $this->testAdminCanUsePrivateTask();
+    $this->deleteRound();
+  }
+
+  private function createRound(): void {
+    $this->login('intern', '1234');
+    $this->clickLinkByText('Concursuri virtuale');
+    $this->clickLinkByText('Creează concurs nou');
+    $this->changeInput('#form_id', 'new-round');
+    $this->clickButton('Creează runda');
+    $this->assertTextExists('Am creat runda, acum poți să o editezi.');
+    $this->clickLinkByText('Parametri');
+  }
+
+  private function testInternCannotUsePrivateTask(): void {
+    $this->getElementByCss('#_dlb1_form_tasks option[value="task1"]');
+    $this->assertNoElement('#_dlb1_form_tasks option[value="task2"]');
+  }
+
+  private function testAdminCanUsePrivateTask(): void {
+    $this->login('admin', '1234');
+    $this->visitRoundEditPage('new-round');
+    $this->clickLinkByText('Parametri');
+
+    $this->getElementByCss('#_dlb1_form_tasks option[value="task1"]');
+    $this->getElementByCss('#_dlb1_form_tasks option[value="task2"]');
+  }
+
+  private function deleteRound(): void {
+    $this->clickButton('Șterge runda');
+
+    $this->deleteRelatedTextblocks();
+
+    $this->clickButton('Șterge runda, Forever...');
+    $this->acceptConfirmationPopup();
+    $this->waitForPageLoad($this->homepageUrl);
+    $this->assertTextExists('Am șters runda.');
+  }
+
+  private function deleteRelatedTextblocks(): void {
+    $checkboxes = $this->getCheckboxesByCss('input[name="textblocks[]"]');
+    $checkboxes->selectByIndex(0);
+    $checkboxes->selectByIndex(1);
+
+    $this->clickButton('Șterge paginile');
+    $this->acceptConfirmationPopup();
+    sleep(1);
+  }
+}
