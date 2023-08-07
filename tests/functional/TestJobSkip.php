4901259da1ded6a6089573a239c730d70de8a899
Cătălin Frâncu <cata@francu.com>
2023-08-07 11:02:26 +0300

    [#61] Test the existence of job ignore links.

---
+++ tests/functional/TestJobSkip.php
@@ -0,0 +1,28 @@
+<?php
+
+class TestJobSkip extends FunctionalTest {
+
+  function run(): void {
+    $this->testInternCannotSkip();
+    $this->testAdminCanSkip();
+  }
+
+  private function testInternCannotSkip(): void {
+    $this->login('intern', '1234');
+    $this->visitMonitorPage();
+    $this->assertNoLink('ignoră');
+    $this->assertNoElement('input[value="Ignoră joburile selectate"]');
+  }
+
+  private function testAdminCanSkip(): void {
+    $this->login('admin', '1234');
+    $this->visitMonitorPage();
+
+    $links = $this->getLinksByText('ignoră');
+    $msg = sprintf('Expected 7 ignore links, found %d.', count($links));
+    $this->assert(count($links) == 7, $msg);
+
+    $this->getElementByCss('input[value="Ignoră joburile selectate"]');
+  }
+
+}
