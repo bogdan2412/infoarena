50a0bdc2028ad36d038386244e67f9bbdd4cb3a1
Cătălin Frâncu <cata@francu.com>
2023-12-23 16:23:02 +0200

    [#58] Fix functional test.

--- tests/functional/TestTaskViewTags.php
+++ tests/functional/TestTaskViewTags.php
@@ -10,8 +10,8 @@ class TestTaskViewTags extends FunctionalTest {
     $this->ensureLoggedOut();
     $this->visitTaskPage('task1');
     $this->clickLinkByText('Arată 2 categorii');
-    $this->clickLinkByText('... 1 tag');
-    $this->clickLinkByText('... 1 tag');
+    $this->clickLinkByText('1 etichete');
+    $this->clickLinkByText('1 etichete');
     $this->assertTextExists('category1');
     $this->assertTextExists('category2');
     $this->assertTextExists('tag1');
